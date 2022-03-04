8c7c00d0e6245891edf6a0ce488817c5f2fb582d
ilimbek003 <kaiyrbekuulualumbek@gmail.com>
2022-03-04 17:16:24 +0300

    first commit

---
+++ src/components/MenuItems.js
@@ -0,0 +1,20 @@
+const MeniItems =[
+    {
+        title:"Home",
+        cName:'nav-link',
+    },
+    {
+        title:"services",
+        cName:'nav-link',
+    },
+    {
+        title:"Products",
+        cName:'nav-link',
+    },
+    {
+        title:"contact us",
+        cName:'nav-link',
+    }
+]
+
+export default MeniItems;
